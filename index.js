dd582787cc07763c63295f7dfd72f0e14aa65f4e
chelle tuerk <tuerkbot@talkingRobot.local>
2021-11-11 18:37:09 -0800

    Console logging 2nd api object endpoints - adjust HTML

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 $(document).ready(() => {
   getUsers()
+  handleClick()
 })
 
-let getUsers = () => {
+const getUsers = () => {
   fetch('https://jsonplaceholder.typicode.com/users/')
+  .then(response => response.json())
+  .then(json =>
+    renderUsers(json))
+}
+
+const renderUsers = (json) => {
+  json.map((user) => {
+    $('.users').append(`
+      <button
+        id=${user.id}
+        class="user">${user.name}</br>
+      </button>`)
+  })
+}
+
+const handleClick = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts/')
   .then(response => response.json())
   .then(json =>
     console.log(json))
