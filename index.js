b44254fce4a69a5222d29eef2c5d412e6c5d5d29
chelle tuerk <tuerkbot@talkingRobot.local>
2021-11-11 19:18:52 -0800

    render posts to each user by clicking on userName button

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+
 $(document).ready(() => {
   getUsers()
-  handleClick()
+  renderUserPosts()
 })
 
-const getUsers = () => {
+let getUsers = () => {
   fetch('https://jsonplaceholder.typicode.com/users/')
   .then(response => response.json())
   .then(json =>
     renderUsers(json))
+}
+
+const renderUserPosts = () => {
+  handleClick()
 }
 
 const renderUsers = (json) => {
@@ -23,6 +28,22 @@ const renderUsers = (json) => {
 const handleClick = () => {
   fetch('https://jsonplaceholder.typicode.com/posts/')
   .then(response => response.json())
-  .then(json =>
-    console.log(json))
+  .then(json => (json.forEach((e) => {
+    $('.users').on('click',(e) => {
+      $('.post').remove()
+      const targetId = e.target.id
+      json.map((e) => {
+        if(e.userId == targetId) {
+          $('.users').append(`
+            <ul>
+              <li
+                id=${e.userId}
+                class="post">
+                </br>${e.body.charAt(0).toUpperCase() + e.body.slice(1)}.</br>
+              </li>
+            </ul>`)
+        }
+      })
+    })
+  })))
 }
